384616b74962c37deb260fcbff0a35dfdab0ae2d
alvarojoao <alvaro@silvino.me>
2015-05-15 18:26:07 -0300

    adicionando melhorias do fb

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index');
 });
+router.post('/', function(req, res, next) {
+  res.render('index');
+});
 
 module.exports = router;
